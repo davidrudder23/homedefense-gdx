2e109f7f6fde2d12bf47e39247d740f4f82596b9
Dave Rudder <drig@noses.org>
2019-05-26 11:48:20 -0600

    Bomber bullets!

--- core/src/org/noses/games/homedefense/tower/BomberTower.java
+++ core/src/org/noses/games/homedefense/tower/BomberTower.java
@@ -1,6 +1,7 @@
 package org.noses.games.homedefense.tower;
 
 import org.noses.games.homedefense.HomeDefenseGame;
+import org.noses.games.homedefense.game.BombBulletShooter;
 import org.noses.games.homedefense.game.NormalBulletShooter;
 import org.noses.games.homedefense.geometry.Point;
 
@@ -8,7 +9,7 @@ public class BomberTower extends Tower {
 
     public BomberTower(HomeDefenseGame parent, double longitude, double latitude) {
         super(parent, "bomber", longitude, latitude,
-                new NormalBulletShooter(parent, 0.8, new Point(latitude, longitude)));
+                new BombBulletShooter(parent, 0.8, new Point(latitude, longitude)));
     }
 
     public static class BomberTowerFactory extends TowerFactory {
